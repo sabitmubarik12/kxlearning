7e9169c4d4c9ed79a6f87a9b2eeee480d6c963d3
sabitmubarik12 <43822013+sabitmubarik12@users.noreply.github.com>
2023-05-10 16:21:44 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
-variable "location" {                               
-  type        = string                              
-  description = "Location"                          
-  default     = "eastus"                            
-}                                                   
-variable "app_name" {                               
-  type        = string                              
-  description = "name of the app"                   
-  default     = "learning"                          
-}                                                   
-variable "state_resource_group_name" {              
-  type        = string                              
-  description = "name of the app"                   
-  default     = "statestorerg"                      
-}                                                   
-variable "state_storage_account_name" {             
-  type        = string                              
-  description = "name of the app"                   
-  default     = "statestoresa"                      
-}                                               
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "learning"
+}
+variable "location" {
+  type        = string
+  description = "Location"
+  default     = "eastus"
+}
+variable "state_resource_group_name" {
+  type        = string
+  description = "name of the app"
+  default     = "statestorerg"
+}
+variable "state_storage_account_name" {
+  type        = string
+  description = "name of the app"
+  default     = "statestoresa"
+}
